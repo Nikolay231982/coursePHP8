dacb844c2d4960b2bf018f272f8a82bec65da3da
Nikolay Malakhov <sobaka@gmail.com>
2023-01-19 13:46:29 +0300

    finished 19.01.23

--- testFile.php
+++ testFile.php
@@ -1,16 +1,5 @@
 <?php
 
-switch( condition) {
-	case 1: echo ""; break;
-	case 1: echo ""; break;
-	default: echo
-}
 
-$q = match($d){
-	1 => "ffff";
-	2 => "ggggg";
-	default=>"fdfdfdfdf";
-}
-echo $q;
 
 
