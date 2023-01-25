e9094ea3d4ca46341cd1314031ea906319f7867c
Nikolay Malakhov <malakhov1982@gmail.com>
2023-01-25 15:13:44 +0300

    finished 25.01.23

---
+++ 25 - Отправка форм. POST запросы/index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>ФОРМА</title>
+<meta charset="utf-8" />
+</head>
+<body>
+
+<h3>Форма ввода данных</h3>
+
+<form action='user.php' method='POST'>
+	<p>Имя: <input type="text" name="name"></p>
+	<p>Возраст: <input type="number" name="age"></p>
+	<input type="submit" value='Отправить'>
+</form>
+
+
+
+
+</body>
+</html>
+
+
+
+
+
+
+
+
